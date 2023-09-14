5f30b00230e6453f5cd9106a6d00a39714af9977
mdadc <mdacc@acc.cc>
2023-09-14 16:00:16 +0700

    针对引用独立返回

--- config.go
+++ config.go
@@ -45,7 +45,37 @@ func Regist[T any](file_identifier string, fn item.LoadFunc[T], opts ...*options
 // flag 最多只有一个值，不存在的时候，获取的是非正则匹配的时候
 // flag 只有一个值的时候，是正则匹配的提取物的下划线作为连接符号的标志
 // 可变参数，纯粹为了美观
-func Get[T any](flags ...string) T {
+// 返回值必须指针，否则失去其意义
+func Get[T any](flags ...string) *T {
+	if len(flags) >= 2 {
+		panic("params  most one")
+	}
+	var flag string
+	if len(flags) == 1 {
+		flag = flags[0]
+	}
+	var a T
+	rt := reflect.TypeOf(a)
+	if v := default_config_mgr.GetLoadItem(rt, flag); v != nil {
+		return (*T)(atomic.LoadPointer(&v.V))
+	}
+	switch rt.Kind() {
+	case reflect.Map:
+		m := reflect.MakeMap(rt).Interface().(T)
+		return &m
+	case reflect.Slice:
+		m := reflect.MakeSlice(rt, 0, 0).Interface().(T)
+		return &m
+	case reflect.Chan:
+		m := reflect.MakeChan(rt, 0).Interface().(T)
+		return &m
+	}
+	return &a
+}
+
+// 针对引用类型，不返回其指针
+// 专门针对map,slice,chan使用
+func GetRef[T any](flags ...string) T {
 	if len(flags) >= 2 {
 		panic("params  most one")
 	}
